0d6fb4c46d3ac2fcae1964016dbc8b7decb37ea0
Yu Wan <wanyuac@126.com>
2022-02-19 22:11:11 +0000

    Release code

---
+++ R/phylo__distsPerClade.R
@@ -0,0 +1,34 @@
+#' @title Summarising phylogenetic distances for each clade in a rooted bifurcating phylogenetic tree
+#' @description This function extracts clades from a rooted bifurcating phylogenetic tree and reports
+#' the minimum, median, and the maximum of phylogenetic distances of each clade. Phylogenetic distances
+#' may be SNP counts (neighbour-joining tree), number of substitutions per site (maximum-likelihood tree),
+#' and so forth. The input tree will be midpoint rooted if it is unrooted.
+#' @param t An input bifurcating phylogenetic tree
+#' @param dec Number of decimals kept for distance summaries
+#' @returns A data frame summarising phylogenetic distances for each clade
+#' @export
+# (C) Copyright 2022 Yu Wan <wanyuac@126.com>
+# Licensed under the Apache License, Version 2.0
+# Release: 19 Feb 2022
+
+distsPerClade <- function(t, dec = 8) {
+    require(ape)
+    require(phytools)
+    if (! is.rooted(t)) {
+        print("The input tree is unrooted and hence is rerooted using the midpoint.")
+        t <- ladderize(phy = midpoint.root(t), right = FALSE)
+    }
+    taxa_num <- length(t$tip.label)
+    nodes_in <- (taxa_num + 1) : (taxa_num + t$Nnode)  # IDs of internal nodes
+    D <- do.call(rbind.data.frame, lapply(nodes_in, .summariseDistsPerClade, tr = t))
+    D$D_min <- round(D$D_min, digits = dec)
+    D$D_max <- round(D$D_max, digits = dec)
+    D$D_median <- round(D$D_median, digits = dec)
+    return(D)
+}
+
+.summariseDistsPerClade <- function(i, tr) {  # i: index of an internal node; t: a phylo object
+    c <- ape::extract.clade(phy = tr, node = i)  # The clade is a sub-tree rooted on the chosen internal node.
+    return(data.frame(Node_in = i, Taxa_num = length(c$tip.label), D_min = min(c$edge.length),
+                      D_max = max(c$edge.length), D_median = median(c$edge.length)))
+}
