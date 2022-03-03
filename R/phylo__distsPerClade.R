4c6dde803d1705270c81c17c3f5f02df891d19d3
Yu Wan <wanyuac@126.com>
2022-03-03 20:45:34 +0000

    Fix a bug

--- R/phylo__distsPerClade.R
+++ R/phylo__distsPerClade.R
@@ -4,14 +4,15 @@
 #' may be SNP counts (neighbour-joining tree), number of substitutions per site (maximum-likelihood tree),
 #' and so forth. The input tree will be midpoint rooted if it is unrooted.
 #' @param tr An input bifurcating phylogenetic tree
+#' @param m A distance matrix with row and column names matching to tip labels of the input tree
 #' @param dec Number of decimals kept for distance summaries
 #' @returns A data frame summarising phylogenetic distances for each clade
 #' @export
 # (C) Copyright 2022 Yu Wan <wanyuac@126.com>
 # Licensed under the Apache License, Version 2.0
-# Release: 19 Feb 2022
+# Release: 19 Feb 2022; last update: 3/3/2022
 
-distsPerClade <- function(tr, dec = 8) {
+distsPerClade <- function(tr, m, dec = 8) {
     require(ape)
     require(phytools)
     if (! is.rooted(tr)) {
@@ -20,15 +21,21 @@ distsPerClade <- function(tr, dec = 8) {
     }
     taxa_num <- length(tr$tip.label)
     nodes_in <- (taxa_num + 1) : (taxa_num + tr$Nnode)  # IDs of internal nodes
-    D <- do.call(rbind.data.frame, lapply(nodes_in, .summariseDistsPerClade, tr = tr))
+    D <- do.call(rbind.data.frame, lapply(nodes_in, .summariseDistsPerClade, tr = tr, m = m))
     D$D_min <- round(D$D_min, digits = dec)
     D$D_max <- round(D$D_max, digits = dec)
-    D$D_median <- round(D$D_median, digits = dec)
     return(D)
 }
 
-.summariseDistsPerClade <- function(i, tr) {  # i: index of an internal node; t: a phylo object
+.summariseDistsPerClade <- function(i, tr, m) {  # i: index of an internal node; tr: a phylo object; m: a distance matrix
     c <- ape::extract.clade(phy = tr, node = i)  # The clade is a sub-tree rooted on the chosen internal node.
-    return(data.frame(Node_in = i, Taxa_num = length(c$tip.label), D_min = min(c$edge.length),
-                      D_max = max(c$edge.length), D_median = median(c$edge.length)))
+    if (is.null(m)) {
+        m_c <- ape::cophenetic.phylo(c)
+    } else {
+        samples <- c$tip.label
+        m_c <- m[samples, samples]
+    }
+    diag(m_c) <- NA  # Mask diagnoal entries
+    return(data.frame(Node_in = i, Taxa_num = length(samples), D_min = min(m_c, na.rm = TRUE),
+                      D_max = max(m_c, na.rm = TRUE)))
 }
