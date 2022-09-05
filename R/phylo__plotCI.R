5894d0b9ff428bb85164330ac337048ffb61e0af
Yu Wan <wanyuac@126.com>
2022-09-05 10:37:07 +0100

    Minor update

--- R/phylo__plotCI.R
+++ R/phylo__plotCI.R
@@ -7,14 +7,14 @@
 #' @export
 # (C) Copyright 2022 Yu Wan <wanyuac@126.com>
 # Licensed under the Apache License, Version 2.0
-# Release: 3 Sep 2022; last update: 4 Sep 2022
+# Release: 3 Sep 2022; last update: 5 Sep 2022
 
 plotCI <- function(x, years, ...) {  # Modified from plot.resBactDating in methods.R of the package
     require(ape)
 
     CI_range <- c(min(x$CI), max(x$CI))
     #xl <- range(pretty(CI_range - x$tree$root.time))
-    xl <- round(range(years) - x$tree$root.time)
+    xl <- round(range(years) - x$tree$root.time)  # Coordinates in the plot are based on the root time whose x is set to zero.
 
     # Draw the tree
     plot.phylo(x$tree, x.lim = xl, ...)  # This function does not draw any axis.
@@ -27,7 +27,7 @@ plotCI <- function(x, years, ...) {  # Modified from plot.resBactDating in metho
     transblue <- grDevices::rgb(0, 0, 1, 0.4)
     transred <- grDevices::rgb(1, 0, 0, 0.4)
     for(i in 1 : (Nnode(x$tree) + Ntip(x$tree)))
-        if (x$CI[i,1] != x$CI[i, 2]) {
+        if (x$CI[i, 1] != x$CI[i, 2]) {
             lines(x = c(x$CI[i, 1], x$CI[i, 2]) - x$tree$root.time,
                   y = rep(obj$yy[i], 2), lwd = 4,lend = 0,
                   col = ifelse(i <= Ntip(x$tree), transred, transblue))
